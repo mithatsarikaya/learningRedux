e620d425936c5dfeed06a00c926e0877a8aea5a5
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-09-29 18:26:38 +0300

    adding new blog to the global state is succesfull

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
 // import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import type { RootState } from "./store";
 import { useSelector, useDispatch } from "react-redux";
+import { addNewPost } from "./features/posts/postsSlice";
 
 function App() {
+  // type TNewPost = { title: string; content: string };
   let posts = useSelector((state: RootState) => state.posts);
-  return <div>{JSON.stringify(posts)}</div>;
+  const dispatch = useDispatch();
+  let initialPost = {
+    title: "",
+    content: "",
+  };
+  const [newPost, setNewPost] = useState(initialPost);
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    let value = e.currentTarget.value;
+    console.log(e);
+    setNewPost((prevPost) => ({
+      ...prevPost,
+      [e.target.name]: value,
+    }));
+  }
+
+  const date = new Date();
+
+  function addBlog(e: React.FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    dispatch(addNewPost({ ...newPost, id: date.getTime().toString() }));
+    setNewPost(initialPost);
+  }
+
+  console.log(newPost);
+  return (
+    <div>
+      <div>{JSON.stringify(posts)}</div>
+      <input
+        onChange={(e) => handleChange(e)}
+        name="title"
+        type="text"
+        value={newPost.title}
+        placeholder="wrt"
+      />
+      <input
+        onChange={(e) => handleChange(e)}
+        name="content"
+        type="text"
+        value={newPost.content}
+      />
+      <button onClick={addBlog} style={{ display: "block", margin: "0 auto" }}>
+        +
+      </button>
+    </div>
+  );
 }
 
 export default App;
